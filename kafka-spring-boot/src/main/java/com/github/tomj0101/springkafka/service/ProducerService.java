1e9eeaeec23504ca210a7bc463d59c0331a32413
tomj0101 <tomasjimenezsantos@gmail.com>
2019-07-15 21:47:29 -0400

    spring boot kafka consumer producer

---
+++ kafka-spring-boot/src/main/java/com/github/tomj0101/springkafka/service/ProducerService.java
@@ -0,0 +1,21 @@
+package com.github.tomj0101.springkafka.service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProducerService {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProducerService.class);
+    private static final String TOPIC = "topic-example-100";
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        logger.info(String.format("#### -> Producing message -> %s", message));
+        this.kafkaTemplate.send(TOPIC, message);
+    }
+}
